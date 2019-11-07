2ca3392de37d7766cbf7c296b7f26608548cea57
ttnny <ttnny@users.noreply.github.com>
2019-11-07 01:07:04 -0800

    Update first spring project

---
+++ spring-first-project/src/main/kotlin/edu/greenriver/it/springfirstproject/SpringFirstProjectApplication.kt
@@ -0,0 +1,11 @@
+package edu.greenriver.it.springfirstproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringFirstProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringFirstProjectApplication>(*args)
+}
--- spring-first-project/src/main/kotlin/edu/greenriver/it/springfirstproject/controllers/IndexController.kt
+++ spring-first-project/src/main/kotlin/edu/greenriver/it/springfirstproject/controllers/IndexController.kt
@@ -1,14 +1,19 @@
 package edu.greenriver.it.springfirstproject.controllers
 
+import edu.greenriver.it.springfirstproject.repositories.IMessageRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class IndexController {
+class IndexController(val repo: IMessageRepository) {
     // Define a route
-    @RequestMapping(path = ["", "/", "/index", "index.html", "/home"])
-    fun getIndex() : String {
-        // "index" is the name of my template (view)
+    @RequestMapping(path = ["", "/", "/index", "/index.html", "/home"])
+    fun getIndex(model: Model): String {
+        val messages = repo.findAll()
+        model.addAttribute("messages", messages)
+
+        //index is the name of my template (view)
         return "index"
     }
 }
